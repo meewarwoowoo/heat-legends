f6ec6d1755b3e32297585f9e0db80af823389a52
Michael Wood <michael.wood@cabinpressure.co.uk>
2023-11-30 11:16:23 +0000

    Update defaultDriversJSON.js
    Updated Orange Number as moved to 7th position as befits the position in the Heavy Rain expansion.

--- src/assets/json/defaultDriversJSON.js
+++ src/assets/json/defaultDriversJSON.js
@@ -5,8 +5,8 @@ const defaultDriversJSON = [
 	{ "id":"driver_d", "name":"Plata" ,      "number":2 ,  "team":"Silver Team" ,  "colour":"Silver" , "active": true     , "human": false     } ,
 	{ "id":"driver_e", "name":"Azul" ,       "number":10 , "team":"Blue Team" ,    "colour":"Blue"   , "active": true     , "human": false     } ,
 	{ "id":"driver_f", "name":"Verde" ,      "number":5 ,  "team":"Green Team" ,   "colour":"Green"  , "active": true     , "human": false     } ,
-	{ "id":"driver_g", "name":"Blanc" ,      "number":9 ,  "team":"White Team" ,   "colour":"White"  , "active": false    , "human": false     } ,
-	{ "id":"driver_h", "name":"Pomarańcza" , "number":6 ,  "team":"Orange Team" ,  "colour":"Orange" , "active": false    , "human": false     } ,
+	{ "id":"driver_g", "name":"Pomarańcza" , "number":4 ,  "team":"Orange Team" ,  "colour":"Orange" , "active": false    , "human": false     } ,
+	{ "id":"driver_h", "name":"Blanc" ,      "number":9 ,  "team":"White Team" ,   "colour":"White"  , "active": false    , "human": false     } ,
 	{ "id":"driver_i", "name":"Pembe" ,      "number":1 ,  "team":"Pink Team" ,    "colour":"Pink"   , "active": false    , "human": false     } ,
 	{ "id":"driver_j", "name":"Shian" ,      "number":8 ,  "team":"Cyan Team" ,    "colour":"Cyan"   , "active": false    , "human": false     } 
 ];
