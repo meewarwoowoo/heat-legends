1d7baa9d3760589ad5c1ac51a90baf53c611a59b
Michael Wood <michael.wood@cabinpressure.co.uk>
2026-04-10 17:46:10 +0100

    Added defaultDifficulty

--- src/assets/json/defaultConfigJSON.js
+++ src/assets/json/defaultConfigJSON.js
@@ -1,4 +1,4 @@
 const defaultConfigJSON = {
-	"showNumber":true,"showTeams":false,"showColours":false,"showSplitNumbers":true,"showMoreWhite":true,"showFinishOnRace":false,"showGrid":false,"showCards":true,"useNew1963":false
+	"showNumber":true,"showTeams":false,"showColours":false,"showSplitNumbers":true,"showMoreWhite":true,"showFinishOnRace":false,"showGrid":false,"showCards":true,"useNew1963":false,"defaultDifficulty":1
 }
 export default defaultConfigJSON ;
