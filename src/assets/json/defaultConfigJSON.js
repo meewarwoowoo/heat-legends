ee0778814c72c67ef0c39dd12247c99b76c21f73
Michael Wood <michael.wood@cabinpressure.co.uk>
2026-04-13 14:20:36 +0100

    Added Legend Levels to settings

--- src/assets/json/defaultConfigJSON.js
+++ src/assets/json/defaultConfigJSON.js
@@ -1,4 +1,4 @@
 const defaultConfigJSON = {
-	"showNumber":true,"showTeams":false,"showColours":false,"showSplitNumbers":true,"showMoreWhite":true,"showFinishOnRace":false,"showGrid":false,"showCards":true,"useNew1963":false,"defaultDifficulty":1
+	"showNumber":true,"showTeams":false,"showColours":false,"showSplitNumbers":true,"showMoreWhite":true,"showFinishOnRace":false,"showGrid":false,"showCards":true,"useNew1963":false,"legendsLevel":1
 }
 export default defaultConfigJSON ;
