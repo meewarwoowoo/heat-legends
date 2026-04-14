0db9ea7b3dff88117a599ed80b7e5cba7b763ff7
Michael Wood <michael.wood@cabinpressure.co.uk>
2026-04-14 16:32:55 +0100

    Added powerup decks

---
+++ src/assets/json/powerUpDeckJSON.js
@@ -0,0 +1,31 @@
+const powerUpDeckJSON = [
+	{"star": 0 , "rival": 0 } ,	{"star": 0 , "rival": 0 } ,
+	{"star": 0 , "rival": 1 } ,	{"star": 0 , "rival": 1 } ,
+	{"star": 0 , "rival": 2 } ,	{"star": 0 , "rival": 2 } ,
+	{"star": 0 , "rival": 4 } ,	{"star": 0 , "rival": 4 } ,
+	{"star": 0 , "rival": 5 } ,	{"star": 0 , "rival": 5 } ,
+	{"star": 0 , "rival": 6 } ,	{"star": 0 , "rival": 6 } ,
+	{"star": 1 , "rival": 0 } ,	{"star": 1 , "rival": 0 } ,
+	{"star": 1 , "rival": 1 } ,	{"star": 1 , "rival": 1 } ,
+	{"star": 1 , "rival": 2 } ,	{"star": 1 , "rival": 2 } ,
+	{"star": 1 , "rival": 4 } ,	{"star": 1 , "rival": 4 } ,
+	{"star": 1 , "rival": 5 } ,	{"star": 1 , "rival": 5 } ,
+	{"star": 1 , "rival": 6 } ,	{"star": 1 , "rival": 6 } ,
+	{"star": 3 , "rival": 0 } ,	{"star": 3 , "rival": 0 } ,
+	{"star": 3 , "rival": 1 } ,	{"star": 3 , "rival": 1 } ,
+	{"star": 3 , "rival": 2 } ,	{"star": 3 , "rival": 2 } ,
+	{"star": 3 , "rival": 4 } ,	{"star": 3 , "rival": 4 } ,
+	{"star": 3 , "rival": 5 } ,	{"star": 3 , "rival": 5 } 
+	{"star": 3 , "rival": 6 } ,	{"star": 3 , "rival": 6 } ,
+];
+export default powerUpDeckJSON ;
+
+/*
+0	{ "header": "Full Throttle" , "text": "Speed 20 / Move to corner 0." , "short": "full-throttle" } ,
+1	{ "header": "Slipstream" , "text": "Follow Slipstream rules." , "short": "slipstream" } ,
+2	{ "header": "Adjust Speed Limit" , "text": "+2 when passing a corner." , "short": "speed-limit" } ,
+3	{ "header": "Draft" , "text": "Move up to three spaces." , "short": "draft" } ,
+4	{ "header": "Impeding" , "text": "Fill both lanes of the track." , "short": "impeding" } ,
+5	{ "header": "Nimble" , "text": "Move to the right hand side of the racing line." , "short": "nimble" } ,
+6	{ "header": "Reckless" , "text": "May move overone extra corner line." , "short": "reckless" } 
+*/
