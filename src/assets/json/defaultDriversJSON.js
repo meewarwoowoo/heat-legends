e794443218bf67439be86baeea67c25b29502789
Michael Wood <michael.wood@cabinpressure.co.uk>
2026-04-14 16:32:40 +0100

    Added rival to defaults

--- src/assets/json/defaultDriversJSON.js
+++ src/assets/json/defaultDriversJSON.js
@@ -1,16 +1,16 @@
 const defaultDriversJSON = [
-	{ "id":"driver_a", "name":"Schwarz" ,    "number":7 ,  "team":"Black Team" ,   "colour":"Black"  , "active": true     , "human": false     } ,
-	{ "id":"driver_b", "name":"Amarillo" ,   "number":14 , "team":"Yellow Team" ,  "colour":"Yellow" , "active": true     , "human": false     } ,
-	{ "id":"driver_c", "name":"Rosso" ,      "number":3 ,  "team":"Red Team" ,     "colour":"Red"    , "active": true     , "human": false     } ,
-	{ "id":"driver_d", "name":"Plata" ,      "number":2 ,  "team":"Silver Team" ,  "colour":"Silver" , "active": true     , "human": false     } ,
-	{ "id":"driver_e", "name":"Azul" ,       "number":10 , "team":"Blue Team" ,    "colour":"Blue"   , "active": true     , "human": false     } ,
-	{ "id":"driver_f", "name":"Verde" ,      "number":5 ,  "team":"Green Team" ,   "colour":"Green"  , "active": true     , "human": false     } ,
-	{ "id":"driver_g", "name":"Pomarańcza" , "number":4 ,  "team":"Orange Team" ,  "colour":"Orange" , "active": true     , "human": false     } ,
-	{ "id":"driver_h", "name":"Morado" ,     "number":9 ,  "team":"Purple Team" ,  "colour":"Purple" , "active": true     , "human": false     } ,
-	{ "id":"driver_i", "name":"Blanc" ,      "number":11 , "team":"White Team" ,   "colour":"White"  , "active": true     , "human": false     } ,
-	{ "id":"driver_j", "name":"Shian" ,      "number":21 , "team":"Cyan Team" ,    "colour":"Cyan"   , "active": false    , "human": false     } ,
-	{ "id":"driver_k", "name":"Bleikur",     "number":17 , "team":"Pink Team" ,    "colour":"Pink"   , "active": false    , "human": false     } ,
-	{ "id":"driver_l", "name":"Kulay-Balat", "number":8 ,  "team":"Tan Team" ,     "colour":"Tan"    , "active": false    , "human": false     } 
+	{ "id":"driver_a", "name":"Schwarz" ,    "number":7 ,  "team":"Black Team" ,   "colour":"Black"  , "active": true     , "human": false , "rival": false     } ,
+	{ "id":"driver_b", "name":"Amarillo" ,   "number":14 , "team":"Yellow Team" ,  "colour":"Yellow" , "active": true     , "human": false , "rival": true     } ,
+	{ "id":"driver_c", "name":"Rosso" ,      "number":3 ,  "team":"Red Team" ,     "colour":"Red"    , "active": true     , "human": false , "rival": false     } ,
+	{ "id":"driver_d", "name":"Plata" ,      "number":2 ,  "team":"Silver Team" ,  "colour":"Silver" , "active": true     , "human": false , "rival": false     } ,
+	{ "id":"driver_e", "name":"Azul" ,       "number":10 , "team":"Blue Team" ,    "colour":"Blue"   , "active": true     , "human": false , "rival": false     } ,
+	{ "id":"driver_f", "name":"Verde" ,      "number":5 ,  "team":"Green Team" ,   "colour":"Green"  , "active": true     , "human": false , "rival": false     } ,
+	{ "id":"driver_g", "name":"Pomarańcza" , "number":4 ,  "team":"Orange Team" ,  "colour":"Orange" , "active": true     , "human": false , "rival": false     } ,
+	{ "id":"driver_h", "name":"Morado" ,     "number":9 ,  "team":"Purple Team" ,  "colour":"Purple" , "active": true     , "human": false , "rival": false     } ,
+	{ "id":"driver_i", "name":"Blanc" ,      "number":11 , "team":"White Team" ,   "colour":"White"  , "active": true     , "human": false , "rival": false     } ,
+	{ "id":"driver_j", "name":"Shian" ,      "number":21 , "team":"Cyan Team" ,    "colour":"Cyan"   , "active": false    , "human": false , "rival": false     } ,
+	{ "id":"driver_k", "name":"Bleikur",     "number":17 , "team":"Pink Team" ,    "colour":"Pink"   , "active": false    , "human": false , "rival": false     } ,
+	{ "id":"driver_l", "name":"Kulay-Balat", "number":8 ,  "team":"Tan Team" ,     "colour":"Tan"    , "active": false    , "human": false , "rival": false     } 
 ];
 
 export default defaultDriversJSON ;
